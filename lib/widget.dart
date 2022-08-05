1447f574c2de617d431099ff715239190d0ce5de
alok8bb <alok8bb@gmail.com>
2022-08-05 23:28:45 +0530

    feat: add simple progress bar to network image loader

--- lib/widget.dart
+++ lib/widget.dart
@@ -8,7 +8,10 @@ Widget brandName() {
     children: const <Widget>[
       Text(
         "CygnusOS",
-        style: TextStyle(color: Colors.black87, fontFamily: 'Overpass',),
+        style: TextStyle(
+          color: Colors.black87,
+          fontFamily: 'Overpass',
+        ),
         textAlign: TextAlign.center,
       ),
       Text(
@@ -19,8 +22,20 @@ Widget brandName() {
   );
 }
 
-Widget imageLoader(String imgURL, int index) =>
-    Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Image.network(imgURL, fit: BoxFit.cover)
-    );
+Widget imageLoader(String imgURL, int index) => Container(
+    margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+    child: Image.network(
+      imgURL,
+      fit: BoxFit.cover,
+      loadingBuilder: (context, child, loadingProgress) {
+        if (loadingProgress == null) return child;
+        return Center(
+          child: CircularProgressIndicator(
+            value: loadingProgress.expectedTotalBytes != null
+                ? loadingProgress.cumulativeBytesLoaded /
+                    loadingProgress.expectedTotalBytes!
+                : null,
+          ),
+        );
+      },
+    ));
