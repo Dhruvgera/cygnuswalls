c982870326e2a243f24e93139d594e87d98aa0f3
alok8bb <alok8bb@gmail.com>
2022-08-06 22:02:01 +0530

    feat: set wallpaper method

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:async_wallpaper/async_wallpaper.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:walls/widget.dart';
 import 'package:flutter/services.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -44,46 +42,30 @@ addfunc() {
 }
 
 
+setWallpaper(wallpaperURL, int screen) async {
+  var dir = await getTemporaryDirectory();
+  String savePath = "${dir.path}/wall.png";
+  log(savePath);
 
-downloadfunc (downloadurl) async {
-  /* WidgetsFlutterBinding.ensureInitialized();
-  await FlutterDownloader.initialize(debug: true, ignoreSsl: true);
-  final taskId = await FlutterDownloader.enqueue(
-    url: downloadurl,
-    savedDir: '/sdcard/download/',
-    showNotification: true, // show download progress in status bar (for Android)
-    openFileFromNotification: true, // click on notification to open downloaded file (for Android)
-  ); */
-  Map<Permission, PermissionStatus> statuses = await [
-    Permission.storage,
-    //add more permission to request here.
-  ].request();
+  try {
+    await Dio().download(
+      wallpaperURL,
+      savePath,
+      onReceiveProgress: (count, total) async {
+        String progressString = "${((count / total) * 100).toStringAsFixed(
+            0)}%";
+        if (progressString == "100%") {
+          await AsyncWallpaper.setWallpaperFromFile(
+              filePath: savePath, wallpaperLocation: screen, goToHome: true);
+        }
+      },
+    );
+  } on DioError catch (e) {
+    log(e.message);
+  }
 
-    var dir = await getTemporaryDirectory();
-    if(dir != null){
-      String savename = "walltobeapplied.png";
-      String savePath = dir.path + "/$savename";
-      print(savePath);
-      //output:  /storage/emulated/0/Download/banner.png
-
-      try {
-        await Dio().download(
-            downloadurl,
-            savePath,
-            onReceiveProgress: (received, total) {
-              if (total != -1) {
-                print((received / total * 100).toStringAsFixed(0) + "%");
-                //you can build progressbar feature too
-              }
-            });
-        print("File is saved to download folder.");
-        //AsyncWallpaper.setWallpaperFromFile(
-        //    savePath, AsyncWallpaper.HOME_SCREEN);
-      } on DioError catch (e) {
-        print(e.message);
-      }
-      return savePath;
-    }
+  return
+    savePath;
 }
 
 
@@ -114,19 +96,22 @@ class FullscreenSlider extends StatelessWidget {
                     reverse: false,
                     viewportFraction: 1,
                     enlargeCenterPage: false,
-                    height: MediaQuery.of(context).size.height)),
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height)),
             Positioned(
                 bottom: 0,
                 right: 1,
                 child: Container(
                   margin:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                   child: FloatingActionButton(
                     backgroundColor: Colors.green,
                     foregroundColor: Colors.white,
-                    onPressed: () => {
-                      //log("URL: ${imgList[imgIndex]}")
-                      downloadfunc(imgList[imgIndex])
+                    onPressed: () =>
+                    {
+                      setWallpaper(imgList[imgIndex], AsyncWallpaper.BOTH_SCREENS)
                     },
                     child: const Icon(Icons.download),
                   ),
